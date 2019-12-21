2e077b231d690fd8d4a354f260e6bf5db727c321
Ondrej Barci <ondrej.barci@ferratum.com>
2019-12-21 17:32:29 +0100

    add heroku support

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/{{json2jsonpath-fe}}'));
+// Serve static files....
+app.use(express.static(__dirname + '/dist/json2jsonpath-fe'));
 
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname,'/dist/{{json2jsonpath-fe}}/index.html'));
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/json2jsonpath-fe/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
--- src/app/jsonpath.service.ts
+++ src/app/jsonpath.service.ts
@@ -1,20 +1,21 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders, HttpResponse} from '@angular/common/http';
-import { Observable } from "rxjs/internal/Observable";
-import {environment} from "../environments/environment";
- 
+import {Observable} from 'rxjs/internal/Observable';
+import {environment} from '../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class JsonpathService {
 
   httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
   };
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
-  generateJsonPaths(json : String): Observable<Object>{
-    return this.http.post<String>(environment.baseUrl + '/json2jsonPath', json, this.httpOptions).pipe();
+  generateJsonPaths(json: string): Observable<Object> {
+    return this.http.post<string>(environment.baseUrl + '/json2jsonPath', json, this.httpOptions).pipe();
   }
 }
